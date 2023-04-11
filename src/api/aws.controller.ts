28dd63deeb60eb2bbfbe57ce1da12882e1546f88
TShabatura <tarasshabatura@gmail.com>
2023-04-11 18:33:29 +0300

    VPC test

--- src/api/aws.controller.ts
+++ src/api/aws.controller.ts
@@ -59,13 +59,66 @@ class AWSController{
         });
       };
 
+    getVpcList = (vpcId: string): Promise<EC2.VpcList> => {
+        return new Promise((resolve, reject) => {
+          const params = {
+            VpcIds: [vpcId],
+          };
+      
+          this.ec2.describeVpcs(params, (err, data) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(data.Vpcs);
+            }
+          });
+        });
+    };
+
+    getSubnet = (subnetId: string): Promise<EC2.Subnet> => {
+        return new Promise((resolve, reject) => {
+          const params = {
+            SubnetIds: [subnetId]
+          };
+      
+          this.ec2.describeSubnets(params, (err, data) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(data.Subnets[0]);
+            }
+          });
+        });
+    };
+
+    getRouteTable = (subnetId: string): Promise<EC2.RouteTable> => {
+        return new Promise((resolve, reject) => {
+            const params = {
+                Filters: [
+                    {
+                      Name: 'association.subnet-id',
+                      Values: [subnetId],
+                    },
+                  ]
+            };
+        
+            this.ec2.describeRouteTables(params, (err, data) => {
+                if (err) {
+                  console.log(err, err.stack);
+                } else {
+                    resolve(data.RouteTables[0]);
+                }
+            });
+        });
+    }
+
     getInstanceType = (reservation: EC2.Reservation): Promise<string> => {
         return new Promise((resolve) => {
             resolve(reservation.Instances[0].InstanceType);
         });
     };
 
-    getInstanceTags = (reservation: EC2.Reservation, tag: string): Promise<string> => {
+    getInstanceTag = (reservation: EC2.Reservation, tag: string): Promise<string> => {
         return new Promise((resolve) => {
             resolve(reservation.Instances[0].Tags.find(obj => obj.Key === tag).Value);
         });
@@ -82,6 +135,42 @@ class AWSController{
             resolve(reservation.Instances[0].PublicIpAddress);
         });
     };
+
+    getVpcId = (reservation: EC2.Reservation): Promise<string> => {
+        return new Promise((resolve) => {
+            resolve(reservation.Instances[0].VpcId);
+        });
+    };
+
+    getVpcCidrBlock = (vpc: EC2.Vpc): Promise<string> => {
+        return new Promise((resolve) => {
+            resolve(vpc.CidrBlock);
+        });
+    };
+    
+    getVpcTag = (vpc: EC2.Vpc, tag: string): Promise<string> => {
+        return new Promise((resolve) => {
+            resolve(vpc.Tags.find(obj => obj.Key === tag).Value);
+        });
+    };
+
+    getSubnetTag = (vpc: EC2.Subnet, tag: string): Promise<string> => {
+        return new Promise((resolve) => {
+            resolve(vpc.Tags.find(obj => obj.Key === tag).Value);
+        });
+    };
+
+    hasNatGateway = (routeTable: EC2.RouteTable): Promise<boolean> => {
+        return new Promise(resolve => {
+            resolve(routeTable.Routes[0].GatewayId.startsWith("nat-"));
+        });
+    }
+
+    hasInternetGateway = (routeTable: EC2.RouteTable): Promise<boolean> => {
+        return new Promise(resolve => {
+            resolve(routeTable.Routes[0].GatewayId.startsWith("igw-"));
+        });
+    }
 }
 
 export default new AWSController();
--- src/tests/api.test.ts
+++ src/tests/api.test.ts
@@ -4,9 +4,9 @@ import awsController from '../api/aws.controller';
 import { EC2 } from 'aws-sdk';
 import cloudxinfoSchema from '../jsonSchemas/cloudxinfoSchema.json';
 
-const API_ENDPOINT = 'http://ec2-3-124-190-77.eu-central-1.compute.amazonaws.com/';
+const API_ENDPOINT = 'http://ec2-52-59-146-252.eu-central-1.compute.amazonaws.com/';
 const REGION = 'eu-central-1';
-const instanceId = 'i-0eafe28032936d448';
+const publicInstanceId = 'i-083afea812d6c6808';
 const accountId = '035825444739'
 
 describe('Deployment validation', () => {
@@ -14,11 +14,23 @@ describe('Deployment validation', () => {
   let inctanceList: EC2.ReservationList;
   let privateInstance: EC2.Reservation;
   let publicInstance: EC2.Reservation;
+  let publicVpcList: EC2.VpcList;
+  let privateVpcList: EC2.VpcList;
+  let publicVpcId: string;
+  let privateVpcId: string;
+  let privateInstanceSubnet: EC2.Subnet;
+  let publicInstanceSubnet: EC2.Subnet;
 
   beforeAll(async() => {
     inctanceList = await awsController.getAllInstances(accountId);
-    privateInstance = inctanceList[1];
+    privateInstance = inctanceList[2];
     publicInstance = inctanceList[0];
+    publicVpcId = await awsController.getVpcId(publicInstance);
+    privateVpcId = await awsController.getVpcId(privateInstance);
+    publicVpcList = await awsController.getVpcList(publicVpcId);
+    privateVpcList = await awsController.getVpcList(privateVpcId);
+    privateInstanceSubnet = await awsController.getSubnet(privateInstance.Instances[0].SubnetId);
+    publicInstanceSubnet = await awsController.getSubnet(publicInstance.Instances[0].SubnetId);
   });
 
   test('Instance type validation', async () => {
@@ -27,27 +39,63 @@ describe('Deployment validation', () => {
   });
 
   test('Instance tags validation', async () => {
-    expect(await awsController.getInstanceTags(publicInstance, "Name")).toBe("cloudxinfo/PublicInstance/Instance");
-    expect(await awsController.getInstanceTags(privateInstance, "Name")).toBe("cloudxinfo/PrivateInstance/Instance");
-    expect(await awsController.getInstanceTags(publicInstance, "cloudx")).toBe("qa");
-    expect(await awsController.getInstanceTags(privateInstance, "cloudx")).toBe("qa");
+    expect(await awsController.getInstanceTag(publicInstance, "Name")).toBe("cloudxinfo/PublicInstance/Instance");
+    expect(await awsController.getInstanceTag(privateInstance, "Name")).toBe("cloudxinfo/PrivateInstance/Instance");
+    expect(await awsController.getInstanceTag(publicInstance, "cloudx")).toBe("qa");
+    expect(await awsController.getInstanceTag(privateInstance, "cloudx")).toBe("qa");
   });
 
   test('Instance OS validation', async () => {
     expect(await awsController.getOS(publicInstance)).toBe("Linux/UNIX");
     expect(await awsController.getOS(privateInstance)).toBe("Linux/UNIX");
   });
-  
+
   test('Instance publicIP validation', async () => {
     expect(await awsController.getPublicIP(publicInstance)).toBeDefined();
     expect(await awsController.getPublicIP(privateInstance)).toBeUndefined();
+  });
+
+  test('Instance VPC non-default validation', () => {
+    expect(publicVpcList[0].IsDefault).toBe(false);
+    expect(privateVpcList[0].IsDefault).toBe(false);
+  });
+
+  test('Private instance VPC subnet validation', async () => {
+    expect(await awsController.getSubnetTag(privateInstanceSubnet, "aws-cdk:subnet-type")).toBe("Private");
+  });
+
+  test('Public instance VPC subnet validation', async () => {
+    expect(await awsController.getSubnetTag(publicInstanceSubnet, "aws-cdk:subnet-type")).toBe("Public");
+  });
+
+  test('Private instance has NAT Gateway', async () => {
+    expect(await awsController.hasNatGateway(await awsController.getRouteTable(privateInstanceSubnet.SubnetId)));
+  });
+
+  test('Public instance has Internet Gateway', async () => {
+    expect(await awsController.hasInternetGateway(await awsController.getRouteTable(publicInstanceSubnet.SubnetId)));
+  });
+
+  test('Private instance VPC CIDR Block validation', async () => {
+    expect(await awsController.getVpcCidrBlock(privateVpcList[0])).toBe("10.0.0.0/16");
+  });
+
+  test('Public instance VPC CIDR Block validation', async () => {
+    expect(await awsController.getVpcCidrBlock(publicVpcList[0])).toBe("10.0.0.0/16");
+  });
+
+  test('Vpc tags validation', async () => {
+    expect(await awsController.getVpcTag(publicVpcList[0], "Name")).toBe("cloudxinfo/Network/Vpc");
+    expect(await awsController.getVpcTag(privateVpcList[0], "Name")).toBe("cloudxinfo/Network/Vpc");
+    expect(await awsController.getVpcTag(publicVpcList[0], "cloudx")).toBe("qa");
+    expect(await awsController.getVpcTag(privateVpcList[0], "cloudx")).toBe("qa");
   });
 });
 
 describe('Application functional validation', () => {
   test('EC2 instance attributes validation', async () => {
-    const expectedAvailibilityZone = await awsController.getAvailabilityZone(instanceId);
-    const expectedPrivateIP = await awsController.getPrivateIP(instanceId);
+    const expectedAvailibilityZone = await awsController.getAvailabilityZone(publicInstanceId);
+    const expectedPrivateIP = await awsController.getPrivateIP(publicInstanceId);
     
     let response = await axios.get(API_ENDPOINT);
     expect(response.status).toBe(200);
